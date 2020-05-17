fa21221f22af41abeb29ef89362a3e64d5d64785
moonsungchul <moonsuchstar@firemstar.com>
2020-05-17 23:27:01 +0900

    update oauth add

--- bin/FmsOAuthServer.py
+++ bin/FmsOAuthServer.py
@@ -13,6 +13,8 @@ app.debug = True
 app.config['SECRET_KEY'] = 'mmm'
 app.config['GOOGLE_OAUTH2_CLIENT_SECRETS_FILE'] = 'client_secrets.json'
 
+oauth2 = UserOAuth2(app)
+
 @app.route('/version')
 def version():
     return {'versin':'OAuth test version 1'}
@@ -23,8 +25,18 @@ def root():
     return {'versin':'OAuth test version home 1'}
 
 
+@app.route('/info')
+@oauth2.required
+def info():
+    return "Hello, {}{}".format(oauth2.email, oauth2.user_id)
 
-oauth2 = UserOAuth2(app)
+
+@app.route('/islogin')
+def islogin():
+    if oauth2.has_credentials():
+        return "로그인"
+    else:
+        return "로그인 하세요"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5600, debug=True)
